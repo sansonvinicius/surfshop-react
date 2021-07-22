456502bf6aff3075b00161126d5a8bfc69b563d7
sansonvinicius <sansonvinicius@gmail.com>
2021-07-22 20:56:45 -0300

    populando dados de produto e inserindo botão de carrinho

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container } from './style';
+import api from '../../services/api';
+
+interface IProduct{
+  id: number;
+  photo: string;
+  name: string;
+  description: string;
+  price: number;
+}
 
 const Home: React.FC = () => {
+  const[data, setData] = useState<IProduct[]>([])
+  useEffect(() =>{
+    api.get('').then(
+      response => {
+        setData(response.data)
+      }
+    )
+  },[])
+
+  const handleCart = (index: number) => {
+
+    const productStore = JSON.stringify(data[index]);
+    localStorage.setItem(`@Produto-${index}`,productStore)
+    
+  }
+
   return (
-    <div>
-      <h1>Hello Vtex</h1>
-    </div>
+   <Container>
+     <section>
+       { data.map( (prod, index) => (
+          <div className="product-content" key={prod.id}>
+          <img src= {prod.photo} alt ="Longboard" width="200" height="auto"/>
+          <h4>{prod.name}</h4>
+          <span>{prod.description}</span>
+          <h6>{prod.price}</h6>
+          <button onClick={ () => handleCart(index)}>Adicionar ao carrinho</button>
+        </div>
+        ))}
+      
+     </section>
+   </Container>
   );
 }
 
