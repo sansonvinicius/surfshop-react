35fadd6953d59ab790d0a4fe7361aaa90e40930f
sansonvinicius <sansonvinicius@gmail.com>
2021-07-23 16:53:52 -0300

    removendo comentários do arquivo clientes.js

--- src/view/Clientes/clientes.js
+++ src/view/Clientes/clientes.js
@@ -95,16 +95,3 @@ render(){
 } 
 
 
-/* /
-<S.Container>
-<div>
-  <img src="https://i.etsystatic.com/11119734/r/il/4454b0/1983342349/il_570xN.1983342349_ii56.jpg" alt="logo" width="200px" height="auto"/>
- </div>
-<S.Content> 
-      <p>Receba nossas promoções:</p>
-      <S.Input  className="nomeInput" placeholder="Nome" />
-      <S.Input  className="emailInput" placeholder="Email" />
-       <S.Button>Cadastrar</S.Button>
-       <S.LinkHome to="/">Voltar para loja</S.LinkHome>
-</S.Content>
-</S.Container> */
